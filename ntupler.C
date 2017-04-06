105e6b27d81cd2b584c689223cebbcf2790c04fb
Doga Gulhan <dgulhan@dogas-mbp.public.wireless.med.harvard.edu>
2017-04-06 11:20:01 -0400

    ntupler

--- ntupler.C
+++ ntupler.C
@@ -13,16 +13,18 @@ public:
   vector<int> strand;
   IndividFlat(){
   }
-  void set(Individ individ){
-    nMut = individ.getNMuts();
+  void set(Individ *individ){
+    nMut = individ->getNMuts();
+    cout << nMut << endl;
     for(int i = 0; i < nMut; i++){
-      Mutation * mut = individ.getIthMut(i);
+      Mutation * mut = individ->getIthMut(i);
       pos.push_back(mut->getPos());
       baseIn.push_back(mut->getBaseIn());
       baseOut.push_back(mut->getBaseOut());
       strand.push_back(mut->getStrand());
       tribase.push_back(mut->getTribase());
       chrom.push_back(mut->getChrom());
+      cout << "pos = " << pos[i] << "baseIn=" << baseIn[i] << "baseOut=" << baseOut[i] << "strand=" << strand[i] << "tribase=" << tribase[i] << "chrom=" << chrom[i] << endl;
     }
   }
   void reset(){
@@ -32,6 +34,7 @@ public:
     strand.clear();
     tribase.clear();
     chrom.clear();
+    nMut = 0;
   }
 };
 
@@ -46,18 +49,25 @@ int main(int argc, char *argv[]){
 
   TFile * outf = new TFile("outfIndividuals.root","recreate");
   TTree * indTree = new TTree("indTree","tree of individuals and their snvs");
+  cout << 1 << endl;
+  indTree->Branch("nMut", &(individ.nMut),"nMut/I");
+  indTree->Branch("pos", &(individ.pos));
+  indTree->Branch("baseIn", &(individ.baseIn));
+  indTree->Branch("baseOut", &(individ.baseOut));
+  indTree->Branch("chrom",&(individ.chrom));
+  indTree->Branch("strand",&(individ.strand));
+  indTree->Branch("tribase",&(individ.tribase));
   
   for(int i = 0; i < individs.size(); i++){
     individ.reset();
-    individ.set(individs[i]);
-
-    indTree->Branch("nMut", &(individ.nMut),"nMut/I");
-    indTree->Branch("pos", &(individ.pos));
-    indTree->Branch("baseIn", &(individ.baseIn));
-    indTree->Branch("baseOut", &(individ.baseOut));
-    indTree->Branch("chrom",&(individ.chrom));
-    indTree->Branch("strand",&(individ.strand));
-    indTree->Branch("tribase",&(individ.tribase));
+    cout << "nMut" << individs[i].getNMuts() << endl;
+    individ.set(&individs[i]);
+    indTree->Fill();
+    cout << 4 << endl;    
   }
+  cout << 5 << endl;
+  indTree->Write();
+  outf->Close();
+
   return 0;
 }

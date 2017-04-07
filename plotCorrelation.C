5a334137ce40fe0ded0201b280c74f3879606f4d
Doga Gulhan <dgulhan@dogas-mbp.public.wireless.med.harvard.edu>
2017-04-07 11:29:21 -0400

    plot fix

--- plotCorrelation.C
+++ plotCorrelation.C
@@ -5,7 +5,7 @@
 
 void plotCorrelation(){
   TH1D::SetDefaultSumw2();
-  TFile * file = new TFile("test.root");
+  TFile * file = new TFile("testSameBaseIn.root");
   TH1D * hist_MI[22];
   TH1D * hist_CF[22];
   for(int iChrom = 0; iChrom < 22; iChrom++){
@@ -17,6 +17,8 @@ void plotCorrelation(){
   TCanvas * canvas = new TCanvas("canvas","", 6*300, 4*300);
   makeMultiPanelCanvas(canvas,6,4,0.0,0.0,0.25,0.25,0.02);
   //canvas->Divide(6,4);
+  TLine line(0.,1.,1.,1.);
+  line.SetLineWidth(2);
   for(int iChrom = 0; iChrom < 22; iChrom++){
     hist_rat[iChrom] = (TH1D*)hist_CF[iChrom]->Clone(Form("hist_rat_chrom%d",iChrom));
     hist_rat[iChrom]->Divide(hist_MI[iChrom]);
@@ -30,10 +32,14 @@ void plotCorrelation(){
     hist_rat[iChrom]->SetMarkerStyle(20);
     hist_rat[iChrom]->SetMarkerColor(kBlue-3);
     hist_rat[iChrom]->SetLineColor(kBlue-3);
-    hist_rat[iChrom]->SetMaximum(0.999);
-    hist_rat[iChrom]->SetMinimum(0.001);
+    hist_rat[iChrom]->SetMaximum(1.4999);
+    hist_rat[iChrom]->SetMinimum(0.50001);
     hist_rat[iChrom]->Draw();
-    drawText(Form("CHROM %d",iChrom+1),0.25,0.28);
+    line.Draw("same");
+    if(iChrom == 4 || iChrom == 10) drawText(Form("CHROM %d",iChrom+1),0.55,0.08);
+    else if(iChrom == 16) drawText(Form("CHROM %d",iChrom+1),0.35,0.28);
+    else if(iChrom > 16)  drawText(Form("CHROM %d",iChrom+1),0.15,0.28);
+    else  drawText(Form("CHROM %d",iChrom+1),0.15,0.08);
     canvas->cd(iChrom+1)->RedrawAxis();
   } 
   canvas->SaveAs("test.pdf");
